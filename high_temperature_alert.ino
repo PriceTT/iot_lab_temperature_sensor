e456d0cf2007c9babbfafc674d42a53bb365cd71
PriceTT <tt.price@gmail.com>
2022-08-04 18:59:49 +0200

    Fixed connecting to discord

--- high_temperature_alert.ino
+++ high_temperature_alert.ino
@@ -6,7 +6,7 @@
 #include "arduino_secrets.h"
 
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-const char server[] = "outlook.office.com";
+const char server[] = "discord.com";
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 const String discord_webhook = SECRET_WEBHOOK;
@@ -14,10 +14,9 @@ const String teams_webhook = SECRET_WEBHOOK_MS;
 const String discord_tts = SECRET_TTS;
 
 int port = 443;
+int status = WL_IDLE_STATUS;     // the Wifi radio's status
 WiFiSSLClient client;
 HttpClient http_client = HttpClient(client, server, port);
-
-int status = WL_IDLE_STATUS;     // the Wifi radio's status
 
 void setup() {
   //Initialize serial and wait for port to open:
@@ -26,56 +25,61 @@ void setup() {
 
   // attempt to connect to Wifi network:
   while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to network: ");
+    Serial.print("[LOG]: Attempting to connect to network: ");
     Serial.println(ssid);
     // Connect to WPA/WPA2 network:
     status = WiFi.begin(ssid, pass);
 
     // wait 10 seconds for connection:
     delay(10000);
-  }
 
-  // you're connected now, so print out the data:
-  Serial.println("You're connected to the network");
+  }
   
+  Serial.println("[LOG]: You're connected to the network");  
   Serial.println("----------------------------------------");
-  printData();
+  get_wifi_connection_info();
   Serial.println("----------------------------------------");
 }
 
 void loop() {
   
-  // check the network connection once every 10 seconds:
- //delay(10000);
- //printData();
-  Serial.println("making POST request");
-  msg_send("Test1");
+  String content;
+  String content_type;
+  String payload;
+  
+  Serial.print("[LOG]: Attempting to connect to: ");
+  Serial.println(server);
+ 
+  Serial.println("[LOG]: Making POST request");
+  
+  content_type  = "application/json";  
+  content = "Test1";  
+  payload = "{\"content\":\"" + content + "\", \"tts\":" + discord_tts + "}";
+  Serial.print("[LOG]: Formatted payload: ");
+  Serial.println(payload);
+  
+  post_webhook(payload, content_type, discord_webhook);
 
-  Serial.println("Wait 60 seconds");
-  delay(60000);
+  Serial.println("[LOG]: Waiting 600 seconds");
+  delay(600000);
   Serial.println("----------------------------------------");
 }
 
-void msg_send(String content) {
-  Serial.println("[HTTP] Connecting to: ");
-  Serial.println(server);
-  Serial.println("[HTTP] Message: " + content);
-  //Serial.println("[HTTP] TTS: " + discord_tts);
-  //http_client.post(discord_webhook, "application/json", "{\"content\":\"" + content + "\", \"tts\":" + discord_tts + "}");
-  String content_json = "{\"text\":\"" + content + "\"}";
-  Serial.println(content_json);
-  http_client.post(discord_webhook, "application/json", content_json);
+void post_webhook(String content_, String content_type_, String webhook_) {
+
+  http_client.post(webhook_, content_type_, content_);
   // read the status code and body of the response
   int statusCode = http_client.responseStatusCode();
   String response = http_client.responseBody();
 
-  Serial.print("[HTTP] Status code: ");
+  Serial.print("[HTTP]: Status code: ");
   Serial.println(statusCode);
-  Serial.print("[HTTP] Response: ");
+  Serial.print("[HTTP]: Response: ");
   Serial.println(response);
 }
 
-void printData() {
+
+void get_wifi_connection_info() {
   Serial.println("Board Information:");
   // print your board's IP address:
   IPAddress ip = WiFi.localIP();
